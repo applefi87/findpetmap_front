31ec258b6e3bce21922494e8fe56ee4b1f18b04a
applefi87 <55140921+applefi87@users.noreply.github.com>
2024-09-08 17:55:22 +0800

    update imageConfig

--- src/infrastructure/configs/imageConfigs.js
+++ src/infrastructure/configs/imageConfigs.js
@@ -2,5 +2,15 @@ const articleImage = {
   maxAmount: 4,
   maxFileSize: 5
 };
+const normalImage = {
+  quality: 80,
+  width: 1080,
+  height: 1080
+};
+const previewImage = {
+  quality: 60,
+  width: 100,
+  height: 100
+}
 
-export default { articleImage };
+export default { articleImage, normalImage, previewImage };
