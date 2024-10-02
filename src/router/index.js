b00822475c407225666226a7567a9c35195f4bdb
applefi87 <werrtyyui@yahoo.com.tw>
2024-10-02 20:37:38 +0800

    fix: route 可以正常取 ssr

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { route } from 'quasar/wrappers'
+import { useUserStore } from 'stores/user';
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
-import routes from './routes'
-
 /*
  * If not building with SSR mode, you can
  * directly export the Router instantiation;
@@ -11,20 +10,94 @@ import routes from './routes'
  * with the Router instance.
  */
 
-export default route(function (/* { store, ssrContext } */) {
+
+
+export default route(function ({ ssrContext }) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
     : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
 
+  function requireLogin(to, from, next) {
+    let users
+    if (process.env.SERVER) {
+      users = ssrContext
+    } else {
+      users = useUserStore();
+    }
+    if (!!users?.token) {
+      next();
+    } else {
+      next({ path: '/' });
+    }
+  }
+
+  function requireNotLogin(to, from, next) {
+    let users
+    if (process.env.SERVER) {
+      users = ssrContext
+    } else {
+      users = useUserStore();
+    }
+    if (!users?.token) {
+      next();
+    } else {
+      next({ path: '/' });
+    }
+  }
+
+  const routes = [
+    {
+      path: '/article',
+      displayName: 'articleRoute',
+      name: 'articleRoute',
+      component: () => import('layouts/MainLayout.vue'),
+      children: [
+        { path: '', name: "notFound", redirect: '/' },
+        // { path: '', name: 'articles', displayName: 'articles', component: () => import('src/pages/article/ArticleListPage.vue') },
+        { path: 'create', name: 'createArticle', displayName: 'createArticle', component: () => import('src/pages/article/CreateArticlePage.vue'), beforeEnter: requireLogin },
+        { path: ':id', name: 'articleDetail', displayName: 'articleDetail', component: () => import('src/pages/article/ArticleDetailPage.vue') },
+      ]
+    },
+    {
+      path: '/',
+      displayName: 'homeRoute',
+      name: 'homeRoute',
+      component: () => import('layouts/MainLayout.vue'),
+      children: [
+        { path: '', name: 'home', displayName: 'home', component: () => import('src/pages/HomePage.vue') },
+        { path: 'forgetPWD', name: 'forgetPWD', displayName: 'forgetPWD', component: () => import('src/pages/user/ForgetPWDPage.vue'), beforeEnter: requireNotLogin },
+      ]
+    },
+    {
+      path: '/me',
+      displayName: 'me',
+      name: 'me',
+      component: () => import('layouts/MainLayout.vue'),
+      children: [
+        { path: '', name: 'userInfo', displayName: 'userInfo', component: () => import('src/pages/user/SettingPage.vue') },
+        { path: 'article', name: 'myArticle', displayName: 'myArticle', component: () => import('src/pages/article/ArticleListPage.vue') },
+        { path: 'changePWD', name: 'changePWD', displayName: 'changePWD', component: () => import('src/pages/user/ChangePWDPage.vue') },
+        { path: 'edit', name: 'editMe', displayName: '編輯個人資料', component: () => import('src/pages/user/EditInfoPage.vue') },
+      ],
+      beforeEnter: requireLogin
+    },
+    // Handle 404 errors with ErrorNotFound component
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: () => import('src/pages/ErrorNotFound.vue')
+    }
+  ]
+
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
-
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE)
   })
+
 
   return Router
 })
